20c0ee7923eae21b973f23e048086e35ee943470
Felipe Alves <felipealvesguarino@gmail.com>
2023-04-13 20:49:58 -0300

    puxando dados da API

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./MainContent.module.css"
 import { Card } from '../Card/Card'
+import axios from 'axios'
 
 export function MainContent() {
+
+  const [users, setUsers] = useState([])
+
+  const buscaUsuarios = async () => {
+    try {
+      let response = await axios.get('https://fakerapi.it/api/v1/users?_quantity=30');
+      setUsers(response.data.data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    buscaUsuarios()
+  }, [])
+
   return (
     <main className={styles.mainContent}>
-      <Card/>
-      <Card/>
       <Card/>
 
     </main>
