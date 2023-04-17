606e3db80a9878de45c461beea8b2e8829c64715
Felipe Alves <felipealvesguarino@gmail.com>
2023-04-16 22:04:26 -0300

    finalização

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,19 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import styles from "./MainContent.module.css"
 import { Card } from '../Card/Card'
-import axios from 'axios'
 
+import axios from 'axios'
 
 export function MainContent() {
 
   const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
 
   const buscaUsuarios = async () => {
     try {
-      let response = await axios.get('https://fakerapi.it/api/v1/users?_quantity=30');
-      setUsers(response.data.data);
+      let response = await axios.get('https://fakerapi.it/api/v1/users?_quantity=30')
+      setUsers(response.data.data)
     } catch (error) {
-      console.log(error)
+      console.log(error);
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -23,17 +26,20 @@ export function MainContent() {
 
   return (
     <main className={styles.mainContent}>
-      {users.length > 0 && (
-        users.map(user => (
-          <Card
-        imagem={user.image}
-        nomeSobrenome={user.firstname + ' '+ user.lastname}
-        userName={user.username}
-        email={user.email}
-        site={user.website}
-      />
-        ))
-      )}
+      {loading ? (<div className={styles.spinner}></div>) :
+        users.length > 0 && (
+          users.map(user => (
+            <Card
+              imagem={user.image}
+              nomeSobrenome={user.firstname + ' ' + user.lastname}
+              userName={user.username}
+              email={user.email}
+              site={user.website}
+            />
+          ))        
+        )     
+      }
+
     </main>
   )
 }
